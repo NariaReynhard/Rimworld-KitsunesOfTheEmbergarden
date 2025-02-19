b55f95223a4e6c298ebb62471bfb3972355b9d39
NariaReynhard <nariareynhard@gmail.com>
2025-02-19 01:32:16 +0100

    Set up of CSharp env and creation of the buff/withdraw of polymer harmony

---
+++ Kitsunes of the Embergarden/KotECode/KotECode/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace KotECode;
+
+public class Class1
+{
+
+}
---
+++ Kitsunes of the Embergarden/KotECode/KotECode/Source/ModEntry.cs
@@ -0,0 +1,12 @@
+using Verse;
+
+namespace KitsuneOfTheEmbergarden
+{
+    public class ModEntry : Mod
+    {
+        public ModEntry(ModContentPack content) : base(content)
+        {
+            Log.Message("Kitsunes of the Embergarden loaded successfully !");
+        }
+    }
+}
